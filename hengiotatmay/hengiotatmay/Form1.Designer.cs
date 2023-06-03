8993aa975b9bf78669471f8b63ec5769af3d0797
huycuong23 <cuongcuonghuy@gmail.com>
2023-06-03 10:51:04 +0700

    landau

---
+++ hengiotatmay/hengiotatmay/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace hengiotatmay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 235);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = "";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.75F);
+            this.label1.Location = new System.Drawing.Point(11, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 48);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập số phút \r\nhẹn giờ tắt máy tính";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 136);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 85);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(85, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Huỷ hẹn";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "poweredby huycuong23";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 303);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Shutdown timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ hengiotatmay/hengiotatmay/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace hengiotatmay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e){}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string time = textBox1.Text;
+            int timeNumber = int.Parse(time, NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite);
+            timeNumber *= 60;
+            if (time.Length == 0)
+            {
+                MessageBox.Show("Enter the number of seconds");
+            } else
+            {
+                Process Pro = new Process();
+                Pro.StartInfo.UseShellExecute = false;
+                Pro.StartInfo.RedirectStandardInput = true;
+                Pro.StartInfo.RedirectStandardOutput = true;
+                Pro.StartInfo.FileName = @"cmd.exe";
+                Pro.StartInfo.CreateNoWindow = true;
+                Pro.Start();
+                Pro.StandardInput.WriteLine("shutdown -s -t "+ timeNumber);
+                Pro.StandardInput.WriteLine("exit");
+                MessageBox.Show("DONE!\n" + " Your computer will shutdown in " +time+ " minute");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process Pro = new Process();
+            Pro.StartInfo.UseShellExecute = false;
+            Pro.StartInfo.RedirectStandardInput = true;
+            Pro.StartInfo.RedirectStandardOutput = true;
+            Pro.StartInfo.FileName = @"cmd.exe";
+            Pro.StartInfo.CreateNoWindow = true;
+            Pro.Start();
+            Pro.StandardInput.WriteLine("shutdown -a");
+            Pro.StandardInput.WriteLine("exit");
+            MessageBox.Show("Đã huỷ");
+        }
+    }
+}
+
